109401dfbfa517e52512173841a4faa75a362881
Anthony Riley II <riley@anth1y.io>
2020-02-26 22:57:41 -0800

    use socketaddr

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 #![deny(deprecated)]
+#![allow(unused_mut)]
+#![allow(unused_variables)]
 
 use tokio::net::TcpStream;
-use tokio::prelude::*;
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 #[tokio::main]
 async fn main() {
-    let mut stream = TcpStream::connect("127.0.0.1:6142").await.unwrap();
-    println!("created stream");
+    let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 6142);
+    let mut stream = TcpStream::connect(addr).await.unwrap();
 
-    let result = stream.write(b"hello world\n").await;
-    println!("wrote to stream; success={:?}", result.is_ok());
 }
